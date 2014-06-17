74cffe30a8ad30a37b5677329b18aadcbb5fca63
Seng Leat <leatseng@barretts.ecs.vuw.ac.nz>
2014-06-17 23:54:40 +1200

    ff

--- web.js
+++ web.js
@@ -1,190 +1,64 @@
 var express = require('express')
-// , bodyParser = require('body-parser')
-// ,app = express.createServer(express.logger())
-, app = express()
-, pg = require('pg').native
-, fs = require('fs')
-// , connectionString = process.env.DATABASE_URL || "http://intense-harbor-6396.herokuapp.com"
-, connectionString = process.env.DATABASE_URL
-, port = process.env.PORT || 3000
-, client
-// , knox = require('knox')
-, crypto = require("crypto")
-;
-//
-// make express handle JSON and other requests
+ //, http =requrie('http')
+  , app = express()//.createServer(express.logger())
+  , pg = require('pg').native
+  ,fs = require('fs')
+  //,AWS = require('aws-sdk')
+  , passport = require('passport')
+  , LocalStrategy = require('passport-local').Strategy
+  , flash = require('connect-flash') //flash message
+  
+  ,Sequelize = require('sequelize')
+  
+  , start = new Date()
+  , connectionString = process.env.DATABASE_URL
+  , port = process.env.PORT
+  , client;
+//var server = http.createServer(app);
+client = new pg.Client(connectionString);
+client.connect();
+var p = port; 
+
+  
+  
+  // make express handle JSON and other requests
 app.use(express.bodyParser());
 // // serve up files from this directory 
 app.use(express.static(__dirname));
 // // if not able to serve up a static file try and handle as REST invocation
 app.use(app.router);
+// for passport
+app.use(express.cookieParser()); //just for auth
+app.use(express.session({ secret: 'SECRET' }));
+app.use(passport.initialize());
+app.use(passport.session());
+var coords = [
+{ lat: -42.5667, lon: 32.767 }, 
+{ lat: 35.76556, lon: 75.5675 }, 
+{ lat: -35.65756, lon: 20.656 }, 
+{ lat: 67.356756, lon: 45.896 }, 
+{ lat: -50.567, lon:185.3450 }, 
+{ lat: 0, lon: 0 }, 
+{ lat: -40.930, lon: 173.050 }
+]
 
-// AWS stuff
-var AWS_ACCESS_KEY = process.env.AWS_ACCESS_KEY;
-var AWS_SECRET_KEY = process.env.AWS_SECRET_ACCESS_KEY;
-var S3_BUCKET = process.env.S3_BUCKET;
+var form = "<!DOCTYPE HTML><html><body>" +
+"<form method='post' action='/upload' enctype='multipart/form-data'>" +
+"<input type='file' name='image'/>" +
+"<input type='submit' /></form>" +
+"</body></html>";
+  
 
-var amazon_url = "http://s3.amazonaws.com/" + S3_BUCKET;
-
-// attempt to connect to database
-client = new pg.Client(connectionString);
-client.connect();
-
-/// Include ImageMagick
-// var im = require('imagemagick');
-
-app.get('/', function(req, res) {
-	// res.writeHead(200, {'Content-Type': 'text/plain' });
-	// res.end(form);
-	res.sendFile("index.html")
-	// res.send("No.")
-})
-
-/// Post files
-app.post('/upload', function (req, res) {
-	if(!req.body.hasOwnProperty("id") || !req.body.hasOwnProperty("imageName") || !req.body.hasOwnProperty("description") || !req.body.hasOwnProperty("url")) {
-		res.statusCode = 400;
-		return res.send("Error 400: Post syntax incorrect.")
-	}
-	
-	
-
-var experiation = new Date(new Date().getTime() + 1000 * 60 * 5).toISOString();
-
-POLICY_JSON = { "expiration": "2020-12-01T12:00:00.000Z",
-            "conditions": [
-            {"bucket": "exploretasman"},
-            ["starts-with", "$key", ""],
-            {"acl": "public-read"},                           
-            ["starts-with", "$Content-Type", ""],
-            ["content-length-range", 0, 524288000]
-            ]
-          };
-
-    var secret64 = "dG1MRDNQOEl3ZlVic1hxN3Y4NzFldmJaeWplaDE1dkVudk1ZbEZHZw==";
-    var secret = new Buffer(secret64, 'base64').toString('ascii');
-
-    var policy = JSON.stringify(POLICY_JSON);
-
-    var policyBase64 = new Buffer(JSON.stringify(POLICY_JSON), 'utf8').toString('base64');
-
-    // policy = 
-eyJleHBpcmF0aW9uIjoiMjAyMC0xMi0wMVQxMjowMDowMC4wMDBaIiwiY29uZGl0aW9ucyI6W3siYnVja2V0IjoiZXhwbG9yZXRhc21hbiJ9LFsic3RhcnRzLXdpdGgiLCIka2V5IiwiIl0seyJhY2wiOiJwdWJsaWMtcmVhZCJ9LFsic3RhcnRzLXdpdGgiLCIkQ29udGVudC1UeXBlIiwiIl0sWyJjb250ZW50LWxlbmd0aC1yYW5nZSIsMCw1MjQyODgwMDBdXX0= 
-
-	var signature = crypto.createHmac('sha1', secret).update(policyBase64).digest('base64');
-
-var row1 = {
-	id: req.body.id,
-	imageName: req.body.imageName,
-	description: req.body.description,
-	imageURL: req.body.url
-};
-
-var id = req.body.id,
-	imageName= req.body.imageName,
-	description= req.body.description,
-	imageURL= req.body.url
-
-console.log("Received info: ", row1);
-
-client.query("INSERT into tasman_table (userid, imgName, imagedescription, imageurl) VALUES($1, $2, $3, $4)",
-	[id, imageName, description, imageURL],
-	function(err, result) {
-		if (err) {
-			console.log(err);
-		} else {
-			console.log("row inserted: " + id + " " + imageURL)
-		}
-	});
-
-res.send(row1)
-// res.redirect('/');
-	// });
-});
-
-app.get("/url", function (req, res) {
-	// var imageURL =  "https://exploretasman.s3.amazonaws.com/events/1402229247866-icon.png";
-	// var inJSON  = {"url": imageURL};
-
-var query = client.query("SELECT imageurl FROM tasman_table WHERE imagedescription='test d'")
-
-query.on("row", function(result) {
-	console.log(result);
-
-	if(!result) {
-		return res.send("No data found!");
-	}
-
-	else {
-		console.log(result)
-		var json = {"url": result.imageURL};
-		res.send(json);
-	}
-})
-
-	// res.send(inJSON);
-})
-
-var awsKey = "AKIAJJUYC4EAIF7D2XDQ";
-var secret64 = "dG1MRDNQOEl3ZlVic1hxN3Y4NzFldmJaeWplaDE1dkVudk1ZbEZHZw==";
-var secret = new Buffer(secret64, 'base64').toString('ascii');
-var bucket = "exploretasman";
-
-app.get("/get", function (req, res, next) {
-	client.query('select img from tasman_table limit 1',
-		function(err, readResult) {
-			console.log("err", err, "pg readResult", readResult);
-			fs.writeFile('')
-		})
-})
-
-app.post('/upload/photos', function (req, res) {
-
-	var serverPath = 'images/' + req.files.userPhoto.name;
-
-	console.log(__dirname + " /public/" + serverPath)
-
-	fs.rename ( 
-		req.files.userPhoto.path,
-		__dirname + '/public/' + serverPath,
-		function (error) {
-			if(error) {
-				res.send( {
-					error: "Something went wrong!"
-				})
-				return;
-			}
-
-			res.send({
-				path: serverPath
-			});
-		}
-		);
-})
-
-/// Show files
-app.get('/uploads/fullsize/:file', function (req, res){
-	file = req.params.file;
-	var img = fs.readFile( __dirname + "uploads/fullsize/" + file);
-	res.writeHead(200, {'Content-Type': 'image/jpg' });
-	res.end(img, 'binary');
-});
-
-
-app.get('/uploads/thumbs/:file', function (req, res){
-	file = req.params.file;
-	var img = fs.readFile( __dirname + "/uploads/thumbs/" + file);
-	res.writeHead(200, {'Content-Type': 'image/jpg' });
-	res.end(img, 'binary');
-});
-	
-/*
+/*kk
+app.post('/upload', function(req, res) {
 	fs.readFile(req.files.image.path, function (err, data) {
-		// console.log("data", data)
-		// var newData = '\\x' + data;
+		console.log("data", data)
+		var newData = '\\x' + data;
 		// console.log("hex data", data)
+
+var des ="";//description
 		var imageName = req.files.image.name
-/*
+
 		/// If there's an error
 		if(!imageName){
 			console.log("There was an error")
@@ -195,11 +69,8 @@ app.get('/uploads/thumbs/:file', function (req, res){
 		else {
 			var newPath = __dirname + "/uploads/fullsize/" + imageName;
 			var thumbPath = __dirname + "/uploads/thumbs/" + imageName;
-
-			// var newPath = "http://intense-harbor-6396.herokuapp.com/uploads/fullsize/" + imageName;
-			// var thumbPath = "http://intense-harbor-6396.herokuapp.com/uploads/thumbs/" + imageName;
-		  /// write file to uploads/fullsize folder
-		  fs.writeFile(newPath, data, function (err) {
+		  /// write file to uploads/fullsize folder*/
+		  /*fs.writeFile(newPath, data, function (err) {
 
 		  	/// write file to uploads/thumbs folder
 		  	im.resize({
@@ -212,100 +83,242 @@ app.get('/uploads/thumbs/:file', function (req, res){
 		  	});
 
 		  	// console.log((JSON.stringify(req.files)))
-		  	// res.redirect("/uploads/fullsize/" + imageName);
+		  	res.redirect("/uploads/fullsize/" + imageName);
 
-			// res.redirect("/");
-			// res.end();
 		  });
-		});
-/*
-	client.query("INSERT INTO tasman_table (imgName, img) VALUES ($1, $2)", 
-		[imageName, newData],
+//
+	client.query("insert into tasman_table (imgName, img, des) values ($1, $2, $3)", 
+		[imageName, newData, des],
 		function(err, writeResult) {
 			console.log("err", err, "pg writeResult", writeResult)
 		});
 
-		  	res.redirect("/uploads/thumbs/" + imageName);
-/*
-		  	client.query("INSERT INTO tasman_table (imgName, image) VALUES ($1, $2)", 
-		  		[imageName, data],
-		  		function(err, writeResult) {
-		  			console.log("err", err, "pg writeResult", writeResult)
-		  		});*/
-		// }
 
-
-/*
-app.get("/location", function (req, res) {
-	res.send(coords);
+	res.end();
+		}
+	});
 });
 
-//do location stuff
-app.post('/location', function(req, res) {
-	console.log(req.body);
-	if(!req.body.hasOwnProperty('lat') || !req.body.hasOwnProperty('lon')) {
-		res.statusCode = 400;
-		return res.send('Error 400: Post syntax incorrect.');
-	}
-
-	var newCoords = {
-		lat : req.body.lat,
-		lon : req.body.lon
-	};
-
-	coords.push(newCoords);
-	client.query('INSERT INTO tasman_table(text) VALUES($1)', [coords]);
-
-  // should send back the location at this point
-  console.log("Added!");
-  //newCoords.pos = coords.length-1;
-  res.json(true);
-  res.send(newCoords);
-});
-
-app.get('/location/random', function(req, res) {
-	var id = Math.floor(Math.random() * coords.length);
-	var q = coords[id];
-	console.log("Sent!");
-	res.send(q);
+/// Show files
+app.get('/uploads/fullsize/:file', function (req, res){
+	file = req.params.file;
+	var img = fs.readFile( __dirname + "/uploads/fullsize/" + file);
+	res.writeHead(200, {'Content-Type': 'image/jpg' });
+	res.end(img, 'binary');
 });
 
 
-//get user id...
-app.get('/user:id', function(req, res) {
-	res.send("Hello World")
-});
-
-//get photos and save in database...
-app.post("/gallery", function(req, res) {
-
-
-});
-
-//get photos and show in gallery...
-app.get("/gallery", function(req, res) {
-
-
-});
-
-app.get('/get/stats', function(req, res) {
-
-  // client.query('INSERT INTO visits(date) VALUES($1)', [date]);
-
-  query = client.query('SELECT * FROM tasman_table');
-  query.on('row', function(result) {
-  	console.log(result);
-
-  	if (!result) {
-  		return res.send('No data found');
-  	} else {
-  		res.send('data from database: ' + result);
-  	}
-  });
+app.get('/uploads/thumbs/:file', function (req, res){
+	file = req.params.file;
+	var img = fs.readFile( __dirname + "/uploads/thumbs/" + file);
+	res.writeHead(200, {'Content-Type': 'image/jpg' });
+	res.end(img, 'binary');
 });
 */
+//app.get('/', function(req, res) {
+ // var date = new Date();
 
-app.listen(port, function () {
-	console.log('Listening on:', port);
+ // client.query('INSERT INTO visits(date) VALUES($1)', [date]);
+
+ // query = client.query('SELECT COUNT(date) AS count FROM visits WHERE date = $1', [date]);
+ // query.on('row', function(result) {
+  //  console.log(result);
+//
+  //  if (!result) {
+  //    return res.send('No data found');
+  //  } else {
+  //    res.send('Visits today: ' + result.count);
+  //  }
+  //});
+//});
+/*
+passport.use(new LocalStrategy(
+    usernameField: 'email', // use email as user name
+    passwordField: 'passwd'
+  function(username, password, done) {
+    User.findOne({ username: username }, function(err, user) {
+      if (err) { return done(err); }
+      if (!user) {
+        return done(null, false, { message: 'Incorrect username.' });
+      }
+      if (!user.validPassword(password)) {
+        return done(null, false, { message: 'Incorrect password.' });
+      }
+      return done(null, user);
+    });
+  }
+));
+
+
+//redirect & Flash Message
+app.post('/login', 
+  passport.authenticate('local', { successRedirect: '/', // redirecting to homepage if success
+                                   failureRedirect: '/login',
+                                   failureFlash: true })
+);
+
+function userExist(req, res, next) { // need to check when !user
+   // Users.count({   // need to check database if username exist
+     //   username: req.body.username
+    }, function (err, count) {
+        if (count === 0) {
+            next();
+        } else {
+            // req.session.error = "User Exist"
+            res.redirect("/singup");
+        }
+    });
+}
+
+
+req.login(user, function(err) {
+  if (err) { return next(err); }
+  return res.redirect('/users/' + req.user.username);
 });
+
+app.get('/logout', function(req, res){
+  req.logout();
+  res.redirect('/');
+});
+//passport.authenticate('local', { failureFlash: 'Invalid username or password.' });
+//passport.authenticate('local', { successFlash: 'Welcome!' });
+app.listen(port, function() {
+  console.log('Listening on:', port);
+});
+*/
+// or 'sqlite', 'postgres', 'mariadb'
+//  3306, // or 5432 (for postgres)
+
+
+app.get('/' , function(req, res){
+   res.sendfile('testinghtml.html')
+     
+     
+     
+  });
+var p = port ; 
+var sequelize = new Sequelize('login_database', 'username', 'password', {
+      dialect: "postgres", 
+      port:  p 
+    })
+ 
+sequelize
+  .authenticate()
+  .complete(function(err) {
+    if (err) {
+      console.log('Unable to connect to the database:', err)
+    } else {
+      console.log('Connection has been established successfully.')
+    }
+  })
+  
+  var User = sequelize.define('User', {
+  username: Sequelize.STRING,
+  password: Sequelize.STRING
+})
+  User.sync();
+  sequelize.sync({force: true});
+  //var user = User.create({ username: "admin", password: "admin" });
+  // Serialize sessions
+  
+passport.serializeUser(function(user, done) {
+  done(null, user.id);
+});
+
+passport.deserializeUser(function(id, done) {
+  db.User.find({where: {id: id}}).success(function(user){
+    done(null, user);
+  }).error(function(err){
+    done(err, null);
+  });
+});
+
+// Use local strategy to create user account
+passport.use(new LocalStrategy(
+  function(username, password, done) {
+    User.find({ username: username }).success(function(user) {
+      if (!user) {
+        done(null, false, { message: 'Unknown user' });
+      } else if (password != user.password) {
+        done(null, false, { message: 'Invalid password'});
+      } else {
+        done(null, user);
+      }
+    }).error(function(err){
+      done(err);
+    });
+  }
+));
+/*
+var auth = {};
+  auth.localStrategy = new LocalStrategy({
+    username: 'username',
+    password: 'password'
+  },
+
+  function (username, password, done){
+    var User = require('./User').User;
+    User.find({username: username}).success(function(user){
+      if (!user){
+        return done(null, false, { message: 'Nobody here by that name'} );
+      }
+      if (user.password !== password){
+        return done(null, false, { message: 'Wrong password'} );
+      }
+      return done(null, { username: user.username });
+    });
+  }
+);
+
+auth.validPassword = function(password){
+  return this.password === password;
+}
+
+auth.serializeUser = function(user, done){
+  done(null, user);
+};
+
+auth.deserializeUser = function(obj, done){
+  done(null, obj);
+};
+
+var passport = require('passport');
+var AuthController = {
+
+  login: passport.authenticate('local', {
+    successRedirect: '/auth/login/success',
+    failureRedirect: '/auth/login/failure'
+  }),
+
+  loginSuccess: function(req, res){
+    res.json({
+      success: true,
+      user: req.session.passport.user
+    });
+  },
+
+  loginFailure: function(req, res){
+    res.json({
+      success:false,
+      message: 'Invalid username or password.'
+    });
+  },
+
+  logout: function(req, res){
+    req.logout();
+    res.end();
+  },
+};
+*/
+app.listen(port, function() {
+  console.log('Listening on:', port);
+});
+
+
+
+
+
+
+
+
 
