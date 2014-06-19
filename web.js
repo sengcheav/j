ad323d5953ad5f40223d16da190df0d88533474a
Seng Leat <leatseng@barretts.ecs.vuw.ac.nz>
2014-06-20 02:37:16 +1200

    tttttt

--- web.js
+++ web.js
@@ -91,8 +91,8 @@ if (query.row == null ){console.log("NULL"); }else { console.log("not null");}
 	//user.username = row.username; user.password = row.password;
 	var user = {  username: row.username, password: row.password};
 	console.log(user + user.username + user.password+" here userrrr");
-	return fn(null, username);
-  });
+	return fn(null, true);
+  })
   
   return fn(null, null);
 //  if (user == 0 ){ console.log("no user hahahha" + username);}
