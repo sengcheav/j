e357ffa69b2f697dca514697e3d2a3fff0b0c76d
Seng Leat <leatseng@barretts.ecs.vuw.ac.nz>
2014-06-19 22:24:08 +1200

    g26

--- web.js
+++ web.js
@@ -79,7 +79,7 @@ var g = [];
 	client.query("SELECT * from login_database1" , function (err, res){
 		if(err){console.log (err);}
 		else {
-			for(var i =0 ; i<res.row.length ; i++){res.rows[i].username;}
+			for(var i =0 ; i<res.rows.length ; i++){res.rows[i].username;}
 			
 		}
 	});
