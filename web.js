a567d00c9268797ed66a304eeb4a4406a5adb80e
Seng Leat <leatseng@dahra.ecs.vuw.ac.nz>
2014-06-23 15:57:36 +1200

    l

--- web.js
+++ web.js
@@ -185,7 +185,8 @@ app.listen(port, function() {
 //   the request is authenticated (typically via a persistent login session),
 //   the request will proceed.  Otherwise, the user will be redirected to the
 //   login page.
+/*
 function ensureAuthenticated(req, res, next) {
   if (req.isAuthenticated()) { return next(); }
   else {res.redirect('/login');}
-}
+}*/
