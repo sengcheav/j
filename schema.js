4592e1096858751ed32de2309e95e5ea7aadc788
Seng Leat <leatseng@dahra.ecs.vuw.ac.nz>
2014-06-23 14:43:02 +1200

    l

--- schema.js
+++ schema.js
@@ -10,5 +10,5 @@ client.connect();
 //query = client.query('CREATE TABLE imagedatabase (Id text , Image bytea, Description text)');
 //query = client.query('CREATE TABLE tasman_table (userid text, imgName text, imagedescription text, imageurl 
 //text)');
-query = client.query('CREATE TABLE login_database1 (username text , password text)');
+query = client.query('CREATE TABLE login_database12 (username text , password text, Id text )');
 query.on('end', function(result) { client.end(); });
