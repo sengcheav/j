a5165d267ab7e53a2bd0cb5f44940fa1721f991f
Seng Leat <leatseng@dahra.ecs.vuw.ac.nz>
2014-06-23 16:15:57 +1200

    fd

--- web.js
+++ web.js
@@ -145,7 +145,7 @@ passport.deserializeUser(function(username, done) {
 }));
 
 //app.post('/signUp')
-///*
+/*
 app.post('/login', function(req, res, next) {
   passport.authenticate('local', function(err, user, info) {  console.log ("user"+ user + info);
     if (err) { return next(err) }
@@ -162,15 +162,15 @@ app.post('/login', function(req, res, next) {
     });
   })(req, res, next);
 });
-//*/
-/*
+*/
+///*
 app.post('/login',
   passport.authenticate('local', { successRedirect: '/',
                                    failureRedirect: '/login',
                                    failureFlash: true })
 
   );
-*/
+//*/
 app.get('/logout', function(req, res){
   req.logout();
   console.log("out");
